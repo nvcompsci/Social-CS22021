71c6b8422953b006b890a43f0b9633782dbe4858
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2021-02-10 13:47:08 -0500

    actually finish login/create account on server side

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = new express()
-const users = []
+const users = loadData().users
 
 //serve client side files
 app.use(express.static('public'))
@@ -8,20 +8,59 @@ app.use(express.json())
 
 app.post("/login", (req, res) => {
     const user = req.body
-    if (user.username.length >= 4 && user.password.length >= 4) {
-        //save new account on server
-        users.push(user)
-        console.log(users)
+    let userMatch = users.find( (u) => u.username == user.username && u.password == user.password )
+    //Does userMatch exist?
+    if (userMatch) {
         res.send({
-            message: "Your account was successfully created."
+            message: "Successful login!",
+            userMatch
         })
     }
     else {
-        res.status(401)
-        res.send({
-            message: "Username or password is invalid."
-        })
+        if (user.username.length >= 4 && user.password.length >= 4) {
+            //save new account on server
+            const newUser = {
+                id: users.length+1,
+                username: user.username,
+                password: user.password
+            }
+            users.push(newUser)
+            console.log(users)
+            res.send({
+                message: "Your account was successfully created.",
+                newUser
+            })
+        }
+        else {
+            res.status(401)
+            res.send({
+                message: "Username or password is invalid."
+            })
+        }
     }
 })
 
 app.listen(3000, () => console.log("Server started"))
+
+function loadData() {
+    return {
+        users:
+        [
+            {
+                id: 1,
+                username: "jword",
+                password: "hello2021"
+            },
+            {
+                id: 2,
+                username: "tommyinnit",
+                password: "blockgame"
+            },
+            {
+                id: 3,
+                username: "joel15",
+                password: "kirby"
+            }
+        ]
+    }
+}
